1af1a76883559213bd8cf393ac7828dfd56aeb1b
ottojoash <louisjoshbricks@gmail.com>
2023-06-21 20:38:49 +0300

    commit

--- src/view/components/Coverage/CoverageCard.jsx
+++ src/view/components/Coverage/CoverageCard.jsx
@@ -1,62 +1,43 @@
-/* eslint-disable react/jsx-no-target-blank */
-import React, { useEffect, useState } from 'react';
-import {
-    Box,
-    Container,
-    Grid,
-    Card,
-    CardContent,
-    Typography,
-} from '@mui/material';
+import React from 'react';
+import { Box, Container, Grid, Card, CardContent, Typography } from '@mui/material';
 
 const CoverageCard = () => {
-    const [coverages, setCoverages] = useState([]);
+  const dummyCoverages = [
+    { _id: 1, coverageName: 'Coverage 1' },
+    { _id: 2, coverageName: 'Coverage 2' },
+    { _id: 3, coverageName: 'Coverage 3' },
+  ];
 
-    useEffect(() => {
-        fetch('https://fdfn-server-v2.vercel.app/api/v1/readCoverage')
-            .then((response) => response.json())
-            .then((result) => setCoverages(result.data))
-            .catch((error) => console.log('error', error));
-    }, []);
-    return (
-        <>
-            <div className="py-5 mb-5 bg-red-600">
-                <h2 className="text-white text-2xl text-center underline">
-                    Our Coverages
-                </h2>
-            </div>
-            <Container>
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid
-                        container
-                        spacing={{ xs: 2, md: 3 }}
-                        columns={{ xs: 4, sm: 8, md: 12 }}
-                    >
-                        {coverages?.map((coverage) => (
-                            <Grid item xs={2} sm={4} md={4} key={coverage._id}>
-                                <Card
-                                    sx={{
-                                        maxWidth: 345,
-                                        mt: 2,
-                                        backgroundColor: '#7f8c8d',
-                                    }}
-                                >
-                                    <CardContent>
-                                        <Typography
-                                            gutterBottom
-                                            sx={{ color: 'white' }}
-                                        >
-                                            {coverage.coverageName}
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <div className="py-5 mb-5 bg-red-600">
+        <h2 className="text-white text-2xl text-center underline">Our Coverages</h2>
+      </div>
+      <Container>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {dummyCoverages.map((coverage) => (
+              <Grid item xs={2} sm={4} md={4} key={coverage._id}>
+                <Card
+                  sx={{
+                    maxWidth: 345,
+                    mt: 2,
+                    backgroundColor: '#7f8c8d',
+                  }}
+                >
+                  <CardContent>
+                    <Typography gutterBottom sx={{ color: 'white' }}>
+                      {coverage.coverageName}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default CoverageCard;
--- src/view/components/Home/HomePackages/GamingPackages/GamingPackages.jsx
+++ src/view/components/Home/HomePackages/GamingPackages/GamingPackages.jsx
@@ -1,195 +1,169 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
-    Box,
-    Grid,
-    Card,
-    CardContent,
-    Button,
-    Typography,
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  Button,
+  Typography,
 } from '@mui/material';
 import kiteIcon from '../../../../../assets/images/kite.png';
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchData } from './../../../../../redux/features/dataSlice';
 import { Link } from 'react-router-dom';
 
 const GamingPackages = () => {
-    const { data, loading } = useSelector((state) => state.dataReducer);
+  // Dummy data for packages
+  const dummyPackages = [
+    {
+      _id: 1,
+      packageName: 'Package 1',
+      totalMb: 100,
+      feature1: 'Feature 1',
+      feature2: 'Feature 2',
+      feature3: 'Feature 3',
+      feature4: 'Feature 4',
+      price: 50,
+      packageType: 'Gaming',
+    },
+    {
+      _id: 2,
+      packageName: 'Package 2',
+      totalMb: 200,
+      feature1: 'Feature 1',
+      feature2: 'Feature 2',
+      feature3: 'Feature 3',
+      feature4: 'Feature 4',
+      price: 75,
+      packageType: 'Gaming',
+    },
+    {
+        _id: 3,
+        packageName: 'Package 2',
+        totalMb: 200,
+        feature1: 'Feature 1',
+        feature2: 'Feature 2',
+        feature3: 'Feature 3',
+        feature4: 'Feature 4',
+        price: 75,
+        packageType: 'Gaming',
+      },
+    // Add more dummy package data as needed
+  ];
 
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(fetchData('readPackages'));
-    }, [dispatch]);
-
-    const gamingPackage = data.data?.filter(
-        (pack) => pack.packageType === 'Gaming'
-    );
-
-    if (loading) {
-        return <h1 className="text-center my-10">Loading...</h1>;
-    }
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={3}>
-                <Grid container item spacing={4}>
-                    {gamingPackage.map((packageData) => (
-                        <Grid
-                            key={packageData?._id}
-                            item
-                            xs={12}
-                            sm={12}
-                            md={6}
-                            lg={4}
-                            xl={4}
-                        >
-                            <Card sx={{ minHeight: 850, width: '100%' }}>
-                                <CardContent sx={{ p: 0 }}>
-                                    <Box
-                                        sx={{
-                                            backgroundColor: 'black',
-                                            textAlign: 'center',
-                                            p: 1,
-                                            color: 'white',
-                                            display: 'grid',
-                                            justifyContent: 'center',
-                                        }}
-                                    >
-                                        <img src={kiteIcon} alt="" />
-                                        <h2
-                                            style={{ padding: '10px 0' }}
-                                            className="text-2xl"
-                                        >
-                                            {packageData?.packageName}
-                                        </h2>
-                                    </Box>
-                                    <Box
-                                        className="package-content"
-                                        sx={{
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                            alignItems: 'center',
-                                            p: '20px 0 0 0',
-                                        }}
-                                    >
-                                        <Typography
-                                            sx={{ mt: -5, fontSize: '25px' }}
-                                        >
-                                            UPTO
-                                        </Typography>
-                                        <Typography
-                                            sx={{
-                                                fontSize: '100px',
-                                                fontWeight: 600,
-                                            }}
-                                        >
-                                            {packageData?.totalMb}
-                                        </Typography>
-                                        <Typography sx={{ ml: 2 }}>
-                                            <h3>Mbps</h3>
-                                            <p>
-                                                Unlimited <br /> Uses{' '}
-                                            </p>
-                                        </Typography>
-                                    </Box>
-                                    <Box
-                                        sx={{
-                                            p: '30px 20px',
-                                            backgroundColor: 'red',
-                                            borderRadius: '0px 20px 20px 0',
-                                            color: 'white',
-                                        }}
-                                    >
-                                        <Typography
-                                            sx={{
-                                                display: 'flex',
-                                                alignItems: 'start',
-                                            }}
-                                        >
-                                            <AddBoxOutlinedIcon
-                                                color="black"
-                                                sx={{ mr: 2 }}
-                                            />
-                                            {packageData?.feature1}
-                                        </Typography>
-                                        <Typography
-                                            sx={{
-                                                display: 'flex',
-                                                alignItems: 'start',
-                                                mt: 2,
-                                            }}
-                                        >
-                                            <AddBoxOutlinedIcon
-                                                color="black"
-                                                sx={{ mr: 2 }}
-                                            />
-                                            {packageData?.feature2}
-                                        </Typography>
-                                        <Typography
-                                            sx={{
-                                                display: 'flex',
-                                                alignItems: 'start',
-                                                mt: 2,
-                                            }}
-                                        >
-                                            <AddBoxOutlinedIcon
-                                                color="black"
-                                                sx={{ mr: 2 }}
-                                            />
-                                            {packageData?.feature3}
-                                        </Typography>
-                                        <Typography
-                                            sx={{
-                                                display: 'flex',
-                                                alignItems: 'start',
-                                                mt: 2,
-                                            }}
-                                        >
-                                            <AddBoxOutlinedIcon
-                                                color="black"
-                                                sx={{ mr: 2 }}
-                                            />
-                                            {packageData?.feature4}
-                                        </Typography>
-                                    </Box>
-                                    <Box
-                                        sx={{
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                            alignItems: 'center',
-                                            p: '30px 0 ',
-                                        }}
-                                    >
-                                        <Typography sx={{ fontSize: '60px' }}>
-                                            {packageData?.price}
-                                        </Typography>
-                                        <Typography sx={{ ml: 1 }}>
-                                            <h4>Monthly Charge</h4>
-                                            <h2 style={{ color: 'red' }}>
-                                                Taka
-                                            </h2>
-                                        </Typography>
-                                    </Box>
-                                </CardContent>
-                                <Box sx={{ textAlign: 'center' }}>
-                                    <Link
-                                        to={`/registration/${packageData?._id}`}
-                                    >
-                                        <Button
-                                            sx={{ p: '15px 50px' }}
-                                            variant="contained"
-                                        >
-                                            CHOOSE
-                                        </Button>
-                                    </Link>
-                                </Box>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid container item spacing={4}>
+          {dummyPackages.map((packageData) => (
+            <Grid
+              key={packageData._id}
+              item
+              xs={12}
+              sm={12}
+              md={6}
+              lg={4}
+              xl={4}
+            >
+              <Card sx={{ minHeight: 850, width: '100%' }}>
+                <CardContent sx={{ p: 0 }}>
+                  <Box
+                    sx={{
+                      backgroundColor: 'black',
+                      textAlign: 'center',
+                      p: 1,
+                      color: 'white',
+                      display: 'grid',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <img src={kiteIcon} alt="" />
+                    <h2 style={{ padding: '10px 0' }} className="text-2xl">
+                      {packageData.packageName}
+                    </h2>
+                  </Box>
+                  <Box
+                    className="package-content"
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      p: '20px 0 0 0',
+                    }}
+                  >
+                    <Typography sx={{ mt: -5, fontSize: '25px' }}>
+                      UPTO
+                    </Typography>
+                    <Typography sx={{ fontSize: '100px', fontWeight: 600 }}>
+                      {packageData.totalMb}
+                    </Typography>
+                    <Typography sx={{ ml: 2 }}>
+                      <h3>Mbps</h3>
+                      <p>Unlimited</p>
+                      <p>Uses</p>
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      p: '30px 20px',
+                      backgroundColor: 'red',
+                      borderRadius: '0px 20px 20px 0',
+                      color: 'white',
+                    }}
+                  >
+                    <Typography sx={{ display: 'flex', alignItems: 'start' }}>
+                      <AddBoxOutlinedIcon color="black" sx={{ mr: 2 }} />
+                      {packageData.feature1}
+                    </Typography>
+                    <Typography
+                      sx={{ display: 'flex', alignItems: 'start', mt: 2 }}
+                    >
+                      <AddBoxOutlinedIcon color="black" sx={{ mr: 2 }} />
+                      {packageData.feature2}
+                    </Typography>
+                    <Typography
+                      sx={{ display: 'flex', alignItems: 'start', mt: 2 }}
+                    >
+                      <AddBoxOutlinedIcon color="black" sx={{ mr: 2 }} />
+                      {packageData.feature3}
+                    </Typography>
+                    <Typography
+                      sx={{ display: 'flex', alignItems: 'start', mt: 2 }}
+                    >
+                      <AddBoxOutlinedIcon color="black" sx={{ mr: 2 }} />
+                      {packageData.feature4}
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      p: '30px 0 ',
+                    }}
+                  >
+                    <Typography sx={{ fontSize: '60px' }}>
+                      {packageData.price}
+                    </Typography>
+                    <Typography sx={{ ml: 1 }}>
+                      <h4>Monthly Charge</h4>
+                      <h2 style={{ color: 'red' }}>UGX</h2>
+                    </Typography>
+                  </Box>
+                </CardContent>
+                <Box sx={{ textAlign: 'center' }}>
+                  <Link to={`/registration/${packageData._id}`}>
+                    <Button sx={{ p: '15px 50px' }} variant="contained">
+                      CHOOSE
+                    </Button>
+                  </Link>
+                </Box>
+              </Card>
             </Grid>
-        </Box>
-    );
+          ))}
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default GamingPackages;
